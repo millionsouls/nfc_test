b6197a155fce0f26b53e79430cf4519033059f3c
Kevin <36448536+millionsouls@users.noreply.github.com>
2024-11-25 17:54:20 -0500

    init

---
+++ nfc_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nfc_test
+//
+//  Created by Kevin W on 11/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nfc_test/nfc_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  nfc_testApp.swift
+//  nfc_test
+//
+//  Created by Kevin W on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct nfc_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
